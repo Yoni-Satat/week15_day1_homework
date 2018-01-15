78140881206cf93b84f5df4aa2aea808840fc72d
Yoni Satat <yoni.satat@gmail.com>
2018-01-15 16:46:15 +0000

    added MoviesList.js

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MoviesList from './MoviesList.js';
 
 class MoviesContainer extends React.Component {
   render() {
     return(
       <div className="movies-container">
-        I am the MoviesContainer
+        <h1>UK Opening This Week</h1>
+        <MoviesList/>
       </div>
     )
   }
---
+++ src/components/MoviesList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Movie from './Movie.js';
+
+class MoviesList extends React.Component {
+  render() {
+    return(
+      <div className="movies-list">I am the MoviesList</div>
+    )
+  }
+}
+
+export default MoviesList;
