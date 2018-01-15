46170a142a7ea24e51d1c58335a0dcbb9c9b671c
Yoni Satat <yoni.satat@gmail.com>
2018-01-15 16:56:57 +0000

    added Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Movie extends React.Component {
+  render() {
+    return(
+      <div className="movie">
+        <h3>Title</h3>
+        <h3>Showtimes</h3>
+      </div>
+    );
+  }
+}
+export default Movie;
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,7 +4,9 @@ import Movie from './Movie.js';
 class MoviesList extends React.Component {
   render() {
     return(
-      <div className="movies-list">I am the MoviesList</div>
+      <div className="movies-list">
+        <Movie />
+      </div>
     )
   }
 }
