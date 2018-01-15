3f29e52301f3915dbd935d5e41ce3b3c66a158de
Yoni Satat <yoni.satat@gmail.com>
2018-01-15 16:26:44 +0000

    added MoviesContainer.js

---
+++ src/components/MoviesContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MoviesContainer extends React.Component {
+  render() {
+    return(
+      <div className="movies-container">
+        I am the MoviesContainer
+      </div>
+    )
+  }
+}
+
+export default MoviesContainer;
